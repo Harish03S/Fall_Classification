c6c9d411c8be6454bc2958914cb20e702bf9f0f1
sudhar277 <cb.en.u4cse21661@cb.students.amrita.edu>
2025-01-16 10:39:52 +0530

    combined dataset

---
+++ Ankle/check samp.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Load your dataset
+# Replace 'path_to_dataset.csv' with the actual path to your dataset file
+data = pd.read_csv('B:\Fall_Classification\Ankle\Ankle_combined_data.csv')
+
+# Count the number of samples for each label
+label_counts = data['label'].value_counts()
+
+# Print the counts
+print("Number of samples for each label:")
+print(label_counts)
+
